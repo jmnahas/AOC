ff02a69fdcd610f2df4773b96af424d07e7e3cd8
juan <juanmnahas@gmail.com>
2026-08-18 08:42:09 -0300

    Primeros Ejs
    Ej 1 y 2 nada mas.

---
+++ Guia Basica C/ej1.c
@@ -0,0 +1,13 @@
+#include <stdio.h> // Incluye la biblioteca stdio.h
+int main() { // main es el punto de entrada
+    printf("Hola Orga!\n"); // imprime en pantalla
+    return 0; // devuelve un 0 al SO
+}
+
+
+//Que aprendi en este ejercicio?
+
+//Como hacer un print, que existe el include stdio que es un copypaste de funciones basicas
+//Como compilar con gcc, de momento lo hice por consola en un futuro hare un makefile
+// Distintas flags de compilacion que nos permiten tener errores de compilacion
+// Flags: -Wall -Wextra -pedantic
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AOC
 Repo de AOC
+## Contenido
+Van a estar todas las guias de la materia y parciales de practica y mas cosas que haga.
